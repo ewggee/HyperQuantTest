08aa4c6963d335d2057ec7d7ced2f8a65a995e6d
Евгений Кузнецов <egions2006@gmail.com>
2025-07-11 20:45:45 +0300

    feat: Добавить сервис расчёта баланса

---
+++ src/Connector.Core/Services/PortfolioService.cs
@@ -0,0 +1,40 @@
+﻿using Connector.Core.Interfaces;
+
+namespace Connector.Core.Services;
+
+public class PortfolioService
+{
+    private readonly ITestConnector _connector;
+
+    public PortfolioService(ITestConnector connector)
+    {
+        _connector = connector;
+    }
+
+    public async Task<Dictionary<string, decimal>> CalculateBalancesAsync(Dictionary<string, decimal> assets)
+    {
+        var rates = new Dictionary<string, decimal>();
+
+        foreach (var asset in assets.Keys)
+        {
+            var ticker = await _connector.GetTickerAsync($"t{asset}USD");
+            rates[asset] = ticker.LastPrice;
+        }
+
+        decimal totalUsdValue = assets.Sum(asset => asset.Value * rates[asset.Key]);
+
+        var balances = new Dictionary<string, decimal>
+        {
+            ["USDT"] = totalUsdValue
+        };
+
+        foreach (var targetCurrency in assets.Keys)
+        {
+            if (targetCurrency == "USDT") continue;
+
+            balances[targetCurrency] = totalUsdValue / rates[targetCurrency];
+        }
+
+        return balances;
+    }
+}
